ceed58da810cd6aa50b53227433633cf9eb51659
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-06-28 14:40:24 +0000

    Add example and explanation for LeetCode #3: Longest Substring Without Repeating Characters

--- Leet_3.py
+++ Leet_3.py
@@ -8,3 +8,11 @@ Given a string s, find the length of the longest substring without repeating cha
 
 A substring means the characters must be continuous."""
 
+"""Example 1
+
+Input: s = "abcabcbb"
+
+Output: 3
+
+Explanation:
+The longest substring is "abc", so the answer is 3."""
