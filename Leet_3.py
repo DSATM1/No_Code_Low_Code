caf28d6d7fec275d9fec49330bdeb9ed8dfbd1d6
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-06-28 14:40:07 +0000

    Add docstring for LeetCode #3: Longest Substring Without Repeating Characters

---
+++ Leet_3.py
@@ -0,0 +1,10 @@
+"""LeetCode #3 – Longest Substring Without Repeating Characters
+
+Difficulty: Medium
+
+Problem Statement
+
+Given a string s, find the length of the longest substring without repeating characters.
+
+A substring means the characters must be continuous."""
+
