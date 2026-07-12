87ecf8a1b87937f36808ca8a5c0a47cedef37df5
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-07-12 19:24:33 +0530

    Implement palindrome check for an integer

--- Leet_9.py
+++ Leet_9.py
@@ -77,4 +77,25 @@ Negative numbers are never palindromes.
 Save the original number.
 Reverse the digits one by one.
 Compare the reversed number with the original.
+
+    
 Python Code
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        if x < 0:
+            return False
+
+        original = x
+        reverse = 0
+
+        while x > 0:
+
+            digit = x % 10
+
+            reverse = reverse * 10 + digit
+
+            x //= 10
+
+        return original == reverse
