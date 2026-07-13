3ce876fe6ecc179728eef861efd471cf4efaa5be
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-07-13 20:53:32 +0530

    Update Leet_10.py

--- Leet_10.py
+++ Leet_10.py
@@ -160,3 +160,30 @@ At each *, choose one of two options:
 Skip x*.
 Use x* to match one more character.
 Recursive Code
+
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+
+        def dfs(i, j):
+
+            if j == len(p):
+                return i == len(s)
+
+            first_match = (
+                i < len(s)
+                and
+                (s[i] == p[j] or p[j] == ".")
+            )
+
+            if j + 1 < len(p) and p[j + 1] == "*":
+
+                return (
+                    dfs(i, j + 2)
+                    or
+                    (first_match and dfs(i + 1, j))
+                )
+
+            return first_match and dfs(i + 1, j + 1)
+
+        return dfs(0, 0)
