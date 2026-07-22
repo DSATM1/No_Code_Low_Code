79fe6595c654582218d2ab0fa083f418a65c44bf
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-07-22 21:33:57 +0530

    Implement longest common prefix function
    Added a function to find the longest common prefix among an array of strings.

--- Leet_14.py
+++ Leet_14.py
@@ -7,3 +7,19 @@ Problem Statement
 Write a function to find the longest common prefix string among an array of strings.
 
 If there is no common prefix, return an empty string "".
+Example 1
+Input:
+strs = ["flower","flow","flight"]
+
+Output:
+"fl"
+
+Explanation:
+
+flower
+flow
+flight
+
+Common prefix:
+
+fl
