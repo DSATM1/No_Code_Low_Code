7d82848394d6d7301b47f673fe8b04def9dc1956
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-07-22 21:39:54 +0530

    Implement longestCommonPrefix method in Solution class

--- Leet_14.py
+++ Leet_14.py
@@ -156,3 +156,22 @@ l ✔
 o ✖
 
 Stop at the first mismatch.
+
+
+Python Code
+class Solution:
+    def longestCommonPrefix(self, strs):
+
+        if not strs:
+            return ""
+
+        for i in range(len(strs[0])):
+
+            char = strs[0][i]
+
+            for s in strs[1:]:
+
+                if i >= len(s) or s[i] != char:
+                    return strs[0][:i]
+
+        return strs[0]
