9ad7da2c977e74e4d1c1dc3cddc1e84894a0a66d
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-07-22 21:33:40 +0530

    Add problem statement for longest common prefix
    Added problem statement for LeetCode #14.

--- Leet_14.py
+++ Leet_14.py
@@ -2,3 +2,8 @@
 LeetCode #14 – Longest Common Prefix
 
 Difficulty: Easy
+Problem Statement
+
+Write a function to find the longest common prefix string among an array of strings.
+
+If there is no common prefix, return an empty string "".
