d1b413e19cf27cae48dffb881a43b2344775c779
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-07-23 22:54:24 +0530

    Update Leet_15.py

--- Leet_15.py
+++ Leet_15.py
@@ -1,1 +1,15 @@
 
+LeetCode #15 – 3Sum
+
+Difficulty: Medium
+
+Problem Statement
+
+Given an integer array nums, return all unique triplets [nums[i], nums[j], nums[k]] such that:
+
+i != j
+i != k
+j != k
+nums[i] + nums[j] + nums[k] == 0
+
+The solution set must not contain duplicate triplets.
