8f55c4856f7fc52200d02560080a613262e73011
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-07-24 18:55:43 +0530

    Update Leet_16.py

--- Leet_16.py
+++ Leet_16.py
@@ -98,3 +98,36 @@ If:
 Sum < target → move left.
 Sum > target → move right.
 Sum == target → return immediately.
+
+Python Code
+class Solution:
+    def threeSumClosest(self, nums, target):
+
+        nums.sort()
+
+        closest = nums[0] + nums[1] + nums[2]
+
+        n = len(nums)
+
+        for i in range(n - 2):
+
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+
+                total = nums[i] + nums[left] + nums[right]
+
+                if abs(total - target) < abs(closest - target):
+                    closest = total
+
+                if total < target:
+                    left += 1
+
+                elif total > target:
+                    right -= 1
+
+                else:
+                    return total
+
+        return closest
