442a8ccfb9b53896436d22a4a74a368fa3fee606
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-07-25 21:08:14 +0530

    Update Leet_17.py

--- Leet_17.py
+++ Leet_17.py
@@ -10,3 +10,13 @@ Given a string digits containing digits from 2–9, return all possible letter c
 The mapping is the same as on a telephone keypad.
 
 Return the answer in any order.
+Phone Keypad Mapping
+Digit	Letters
+2	    abc
+3	    def
+4	    ghi
+5	    jkl
+6	    mno
+7	    pqrs
+8	    tuv
+9	    wxyz
