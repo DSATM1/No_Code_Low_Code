f65ea7c8ff7ea4996ecefc54929b5a580bafbefb
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-07-25 21:10:28 +0530

    Update Leet_17.py

--- Leet_17.py
+++ Leet_17.py
@@ -104,3 +104,37 @@ Remove the letter (backtrack).
 Store complete combinations.
 
   
+Python Code
+class Solution:
+    def letterCombinations(self, digits):
+
+        if not digits:
+            return []
+
+        phone = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        result = []
+
+        def backtrack(index, path):
+
+            if index == len(digits):
+                result.append(path)
+                return
+
+            letters = phone[digits[index]]
+
+            for letter in letters:
+                backtrack(index + 1, path + letter)
+
+        backtrack(0, "")
+
+        return result
